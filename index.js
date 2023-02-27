1fa2d5fefc795971d0387584a037a76490304bbc
Branaconda <braelli455@gmail.com>
2023-02-27 06:58:53 -0600

    completes assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(spy) {
+    spy();
+}
+
+function returnsANamedFunction() {
+
+    return returnsANamedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+
+    }
+}
